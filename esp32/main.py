d9369d9f1eac67ed1e9ca517e4cff7ce9bd84e88
daniqss <danielqueijo14@gmail.com>
2025-03-28 09:21:10 +0100

    chore(#7): add led on for debugging
    closes #7

--- esp32/main.py
+++ esp32/main.py
@@ -3,13 +3,13 @@ import time
 
 botons = [
     Pin(26, Pin.IN, Pin.PULL_UP),
-#     Pin(25, Pin.IN, Pin.PULL_UP),
-#     Pin(33, Pin.IN, Pin.PULL_UP),
-#     Pin(32, Pin.IN, Pin.PULL_UP),
-#     Pin(35, Pin.IN, Pin.PULL_UP),
-#     Pin(34, Pin.IN, Pin.PULL_UP),
-#     Pin(39, Pin.IN, Pin.PULL_UP),
-#     Pin(36, Pin.IN, Pin.PULL_UP),
+    # Pin(25, Pin.IN, Pin.PULL_UP),
+    # Pin(33, Pin.IN, Pin.PULL_UP),
+    # Pin(32, Pin.IN, Pin.PULL_UP),
+    # Pin(35, Pin.IN, Pin.PULL_UP),
+    # Pin(34, Pin.IN, Pin.PULL_UP),
+    # Pin(39, Pin.IN, Pin.PULL_UP),
+    # Pin(36, Pin.IN, Pin.PULL_UP),
 ]
 notes = [
     "do",
@@ -21,17 +21,21 @@ notes = [
     # "si",
     # "do-sharp",
 ]
+led = Pin(2, Pin.OUT)
 
 def main():
     while True:
         for boton, note in zip(botons, notes):
             if boton.value() == 0:
+                led.on()
 
                 print(note)
                 time.sleep_ms(200)
                 
                 while boton.value() == 0:
                     pass
+
+                led.off()
 
         
         time.sleep_ms(50)
