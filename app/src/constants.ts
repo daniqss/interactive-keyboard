110ada0ab35ea78d326c6f8f1cdd57c069dfab69
daniqss <danielqueijo14@gmail.com>
2025-05-08 17:06:52 +0200

    feat: more accesible images

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -5,28 +5,28 @@ export const ANIMALS: Animal[] = [
   {
     name: "Elefante",
     sound: `${CONFIG.audioPath}/elephant_sound.wav`,
-    image: `${CONFIG.imagePath}/elephant_image.webp`,
+    image: `${CONFIG.imagePath}/elefante.png`,
     imageAlt:
       "Photo by Rachel Claire: https://www.pexels.com/photo/huge-elephant-with-long-trunk-standing-on-green-meadow-4577791/",
   },
   {
     name: "Tigre",
     sound: `${CONFIG.audioPath}/tiger_sound.wav`,
-    image: `${CONFIG.imagePath}/tiger_image.webp`,
+    image: `${CONFIG.imagePath}/tigre.png`,
     imageAlt:
       "Photo by GEORGE DESIPRIS: https://www.pexels.com/photo/tiger-looking-ferocious-2055100/",
   },
   {
     name: "Perro",
     sound: `${CONFIG.audioPath}/dog_sound.wav`,
-    image: `${CONFIG.imagePath}/dog_image.webp`,
+    image: `${CONFIG.imagePath}/perro.png`,
     imageAlt:
       "Photo by Pixabay: https://www.pexels.com/photo/long-coated-white-and-black-dog-220938/",
   },
   {
     name: "Delfín",
     sound: `${CONFIG.audioPath}/dolphin_sound.wav`,
-    image: `${CONFIG.imagePath}/dolphin_image.webp`,
+    image: `${CONFIG.imagePath}/delfin.png`,
     imageAlt:
       "Photo by Coral Grandbois: https://www.pexels.com/photo/close-up-shot-of-dolphins-in-an-aquarium-7316200/",
   },
