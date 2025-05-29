1c16bf3d23d030366a0e226713989608efa975dd
daniqss <danielqueijo14@gmail.com>
2025-05-29 20:46:03 +0200

    fix: change assets path

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
+const base = "/interactive-keyboard/";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-  base: "/interactive-keyboard/",
+  base,
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
@@ -32,7 +33,7 @@ export default defineConfig(async () => ({
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: `assets/[name].[ext]`,
+        assetFileNames: `${base}/assets/[name].[ext]`,
       },
     },
   },
